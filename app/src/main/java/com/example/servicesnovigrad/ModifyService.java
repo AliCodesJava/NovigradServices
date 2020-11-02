51a87cf7866f95fe462b5f70ab5ccff48a132db4
Arlolic <ahcharba@gmail.com>
2020-11-01 23:22:07 -0500

    UI changes, Enum modified

--- app/src/main/java/com/example/servicesnovigrad/ModifyService.java
+++ app/src/main/java/com/example/servicesnovigrad/ModifyService.java
@@ -98,7 +98,7 @@ public class ModifyService extends AppCompatActivity {
         DocumentType newDocType = (DocumentType)documentTypeSpinner.getSelectedItem();
         if(!currentService.getRequiredDocument().contains(newDocType)){
             DatabaseHelper.dbr = DatabaseHelper.setToPath("Services/" + currentService.getServiceType()
-                                 + "/requiredDocuments");
+                                 + "/requiredDocument");
             currentService.addRequiredDoc(newDocType);
             DatabaseHelper.dbr.setValue(currentService.getRequiredDocument());
 

016eede49ec90d43daed059a15017689b86eae1a
AliCodesJava <ali.ottawa.canada@gmail.com>
2020-11-22 14:24:36 -0500

    petit ajout

--- app/src/main/java/com/example/servicesnovigrad/ActivityBranch.java
+++ app/src/main/java/com/example/servicesnovigrad/ActivityBranch.java
@@ -1,13 +1,14 @@
 package com.example.servicesnovigrad;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.time.DayOfWeek;
 
@@ -23,33 +24,26 @@ public class ActivityBranch extends AppCompatActivity {
         intent = getIntent();
         user = (Employee)intent.getSerializableExtra(RegisterForm.EXTRA_USER);
 
-        Spinner spinner = findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerId);
         spinner.setAdapter(new ArrayAdapter<DayOfWeek>(this, android.R.layout.simple_spinner_item, DayOfWeek.values()));
     }
 
-    public void submit(View view){
-        TextView appNum = findViewById(R.id.appNumber);
-        TextView streetNum = findViewById(R.id.streetNumber);
-        TextView streetName = findViewById(R.id.streetName);
-        TextView postalCode = findViewById(R.id.postalCode);
-        TextView city = findViewById(R.id.city);
-        TextView openHrs = findViewById(R.id.openHoursId);
-        TextView closingHrs = findViewById(R.id.closingHours);
+    public void submit(View view) {
+        /*EditText appNum = findViewById(R.id.appNumber);
+        EditText streetNum = findViewById(R.id.streetNumber);
+        EditText streetName = findViewById(R.id.streetName);
+        EditText postalCode = findViewById(R.id.postalCode);
+        EditText city = findViewById(R.id.city);
+        EditText openHrs = findViewById(R.id.openHoursId);
+        EditText closingHrs = findViewById(R.id.closingHours);*/
 
         /*Address branchAddress = new Address(streetNum,
                     Short.parseShort(appNum.getText().toString()), streetName.getText().toString(),
                     city.getText().toString(), postalCode.getText().toString());*/
 
-        WeeklySchedule ws = new WeeklySchedule();
 
-
-        short appartmentNum, openHours, closingHours;
-        try{
-            openHours = Short.parseShort(openHrs.getText().toString());
-            closingHours = Short.parseShort(closingHrs.getText().toString());
-            appartmentNum = Short.parseShort(appNum.getText().toString());
-        }catch(NumberFormatException e){
-
-        }
     }
 }
+
+
+
